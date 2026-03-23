942541bf74b1dc7164b80b899e6d63b3c4cc9615
Loukky <12481807+Loukky@users.noreply.github.com>
2026-03-23 20:33:09 +0800

    Update init.go

--- init.go
+++ init.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	_ "github.com/Loyalsoldier/geoip/plugin/maxmind"
-	_ "github.com/Loyalsoldier/geoip/plugin/plaintext"
-	_ "github.com/Loyalsoldier/geoip/plugin/singbox"
-	_ "github.com/Loyalsoldier/geoip/plugin/special"
-	_ "github.com/Loyalsoldier/geoip/plugin/v2ray"
+	_ "github.com/Loukky/geoip/plugin/maxmind"
+	_ "github.com/Loukky/geoip/plugin/plaintext"
+	_ "github.com/Loukky/geoip/plugin/singbox"
+	_ "github.com/Loukky/geoip/plugin/special"
+	_ "github.com/Loukky/geoip/plugin/v2ray"
 )
