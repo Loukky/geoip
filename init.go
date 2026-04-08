aaa92ddd48d52b8bef56d819471d1c755adc69fc
Loukky <12481807+Loukky@users.noreply.github.com>
2026-04-08 13:00:26 +0800

    修复初始化

--- init.go
+++ init.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	_ "github.com/Loukky/geoip/plugin/maxmind"
+	_ "github.com/Loukky/geoip/plugin/mihomo"
 	_ "github.com/Loukky/geoip/plugin/plaintext"
 	_ "github.com/Loukky/geoip/plugin/singbox"
 	_ "github.com/Loukky/geoip/plugin/special"
